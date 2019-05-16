6af0b9ef99d7d68d7f44a9545d791cbfe8a4dbab
Sara Vieira <hey@iamsaravieira.com>
2019-05-16 15:03:46 +0200

    update seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Humble Brag Meetup`,
+    description: `Watch humble humans struggle as they have to talk about themselves for 8 minutes`,
+    author: `Carolyn Stransky && Sara Vieira`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
